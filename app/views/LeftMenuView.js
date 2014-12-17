795439a6ec3cdeb1613cc12ca471f5ce5d052c71
evgeny <jonny.predein@apiumtech.com>
2014-12-17 13:53:15 +0100

    apps adapter created

--- app/views/LeftMenuView.js
+++ app/views/LeftMenuView.js
@@ -14,50 +14,7 @@ app.registerView(function (container) {
         $scope.fn = this.fn;
 
         this.presenter = $presenter;
-
-        this.fn.toggleAnalyticsSubmenu = function (target) {
-            var element = angular.element(target);
-            if (element.slideToggle) element.slideToggle(250);
-        };
-
-        this.fn.toggleSideBar = function () {
-            console.log('toggleSideBar');
-            var sidebarClass = 'page-sidebar-minified';
-            var targetContainer = '#sidebar';
-            if (angular.element(targetContainer).hasClass(sidebarClass)) {
-                angular.element(targetContainer).removeClass(sidebarClass);
-                if (angular.element(targetContainer).hasClass('page-sidebar-fixed')) {
-                    this.generateSlimScroll(angular.element('#sidebar [data-scrollbar="true"]'));
-                }
-            } else {
-                angular.element(targetContainer).addClass(sidebarClass);
-                if (angular.element(targetContainer).hasClass('page-sidebar-fixed')) {
-                    angular.element('#sidebar [data-scrollbar="true"]').slimScroll({destroy: true});
-                    angular.element('#sidebar [data-scrollbar="true"]').removeAttr('style');
-                }
-                // firefox bugfix
-                angular.element('#sidebar [data-scrollbar=true]').trigger('mouseover');
-            }
-            angular.element(window).trigger('resize');
-
-        };
-
     }
-
-    LeftMenuView.generateSlimScroll = function(element) {
-        var dataHeight = $(element).attr('data-height');
-        dataHeight = (!dataHeight) ? $(element).height() : dataHeight;
-
-        var scrollBarOption = {
-            height: dataHeight,
-            alwaysVisible: true
-        };
-        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            scrollBarOption.wheelStep = 1;
-            scrollBarOption.touchScrollStep = 100;
-        }
-        $(element).slimScroll(scrollBarOption);
-    };
 
     LeftMenuView.prototype.show = function () {
         this.presenter.show(this);
