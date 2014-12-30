7e0c264d370493fbdebe7518d5f20ea2986580f5
Justin Chasez <justin.nguyen@eztek.vn>
2014-12-30 13:12:35 +0700

    Added fake service for moving widget

--- __fakeServer.js
+++ __fakeServer.js
@@ -2,53 +2,66 @@
  * Created by justin on 12/17/14.
  */
 var express = require('express');
+var bodyParser = require('body-parser');
 var port = 8065;
 var mainPath = "__server/assets";
+var delay = 1000;
 
 var app = express();
+app.use(bodyParser());
 app.use(express.static(mainPath));
 
 app.get('/img/*', function (req, res) {
     app.render(mainPath + req.url);
 });
 
-app.get('/api/widgets/intensity', function (request, response) {
+var WidgetService = require(__dirname + "/__server/widgetService");
+app.get('/api/widgets/:page', function (request, response) {
     setTimeout(function () {
-        response.end(JSON.stringify({
+        var page = request.params.page;
+        var widgets = WidgetService.getWidgetFromPage(page);
+        response.json({
             success: true,
-            data: [
-                {
-                    widgetType: "graph",
-                    widgetName: "Widget A",
-                    widgetId: "1",
-                    order: 1,
-                    columns: 1
-                },
-                {
-                    widgetType: "table",
-                    widgetName: "Widget B",
-                    widgetId: "2",
-                    order: 2,
-                    columns: 1
-                }
-            ]
-        }));
-    }, 1000);
+            data: widgets
+        });
+    }, delay);
 });
 
 app.get('/api/widget/:id', function (request, response) {
-    var id = request.params.id;
+    var id = parseInt(request.params.id);
     setTimeout(function () {
-            response.end(JSON.stringify({
-                success: true,
-                data: {
-                    widgetId: id,
-                    params: '{ "img": "http://' + getDomain() + '/img/intensity-chart-' + id + '.jpg" }'
-                }
-            }));
-        },
-        1000
-    );
+        var widget = WidgetService.getWidget(id, request, response);
+
+        if (!widget) {
+            response.status(404);
+            response.json({
+                success: false,
+                error: "Could not find the requested widget"
+            });
+            return;
+        }
+
+        response.json({
+            success: true,
+            data: {
+                widgetId: widget.widgetId,
+                params: '{"img": "http://' + getDomain() + '/img/intensity-chart-' + id + '.jpg"}'
+            }
+        });
+    }, delay);
+});
+
+app.post('/api/widget/:id/move', function (request, response) {
+    var oldIndex = request.body.oldIndex;
+    var newIndex = request.body.newIndex;
+    var result = WidgetService.moveWidget(parseInt(request.params.id), oldIndex, newIndex, request, response);
+
+    if (result) {
+        response.json({
+            success: true
+        });
+        return;
+    }
 });
 
 function getDomain() {
---
+++ __server/widgetService.js
@@ -0,0 +1,87 @@
+/**
+ * Created by justin on 12/30/14.
+ */
+
+var _ = require("underscore");
+
+var widgetService = {};
+var widgetList = [
+    {
+        page: "intensity",
+        widgetType: "graph",
+        widgetName: "Widget A",
+        widgetId: 1,
+        order: 0,
+        columns: 1
+    },
+    {
+        page: "intensity",
+        widgetType: "table",
+        widgetName: "Widget B",
+        widgetId: 2,
+        order: 1,
+        columns: 1
+    },
+    {
+        page: "intensity",
+        widgetType: "table",
+        widgetName: "Widget C",
+        widgetId: 3,
+        order: 2,
+        columns: 1
+    }
+];
+
+widgetService.getWidgetFromPage = function (page) {
+    var pageWidgets = _.clone(_.filter(widgetList, function (widget) {
+        return widget.page === page;
+    }));
+
+    return _.each(pageWidgets, function (widget) {
+        delete widget.page;
+        delete widget.params;
+    });
+};
+
+widgetService.moveWidget = function (widgetId, oldIndex, newIndex, request, response) {
+    var widget = widgetService.getWidget(widgetId);
+    if (widget == null) {
+        response.status(404);
+        response.json({
+            success: false,
+            error: "Could not find requested widget"
+        });
+        return;
+    }
+    var samePageWidget = _.filter(widgetList, function (wg) {
+        return wg.page === widget.page;
+    });
+    samePageWidget = _.sortBy(samePageWidget, function (wg) {
+        return wg.order;
+    });
+
+    if (newIndex >= samePageWidget.length) {
+        var k = newIndex - samePageWidget.length;
+        while ((k--) + 1) {
+            this.push(undefined);
+        }
+    }
+    samePageWidget.splice(newIndex, 0, samePageWidget.splice(oldIndex, 1)[0]);
+    var i = 0;
+    _.each(samePageWidget, function (wg) {
+        wg.order = i;
+        i++;
+    });
+
+    return true;
+};
+
+widgetService.getWidget = function (widgetId) {
+    var widget = _.first(_.filter(widgetList, function (widget) {
+        return widget.widgetId === widgetId
+    }));
+
+    return widget;
+};
+
+module.exports = _.extend(module.exports, widgetService);
