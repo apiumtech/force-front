d7f1a6a8154e8293c71a4ea4c0cefced76aff4e4
evgeny <jonny.predein@apiumtech.com>
2015-04-09 12:01:09 +0200

    grunt keepalive added

--- grunt/tasks/devServer.js
+++ grunt/tasks/devServer.js
@@ -5,5 +5,7 @@ module.exports = function (grunt) {
     'use strict';
 
     //grunt.registerTask('dev-server', ['less:dev', 'http-server:dev', 'watch:less']);
-    grunt.registerTask('dev-server', ['less:dev', 'express:api', 'express-keepalive']);
+    grunt.registerTask('dev-server', ['less:dev', 'express:api', 'keepalive'], function(){
+
+    });
 };
