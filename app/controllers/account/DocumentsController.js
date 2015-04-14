74f6071550e1d84a2007d21facfd3dba449fd345
Justin Chasez <justin.nguyen@eztek.vn>
2015-04-14 14:52:38 +0700

    Preparing for document cmvp

---
+++ app/controllers/account/DocumentsController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by justin on 4/14/15.
+ */
+app.registerController(function () {
+    function DocumentsController($scope) {
+
+    }
+
+    DocumentsController.configureView = function ($scope) {
+
+    };
+
+});
--- app/views/account/AccountView.js
+++ app/views/account/AccountView.js
@@ -113,10 +113,16 @@ app.registerView(function (container) {
                 loadingIndicator: true
             },
             bSort: true,
-            ajax: {
-                url: Configuration.api.dataTableRequest,
-                type: 'POST'
+            ajax: function(data, callback, aa){
+                console.log(data);
+                console.log(callback);
+                console.log(aa);
             },
+
+            //ajax: {
+            //    url: Configuration.api.dataTableRequest,
+            //    type: 'POST'
+            //},
             sDom: "<'row'<'col-md-6 col-sm-6'l><'col-md-6 col-sm-6'f>r>tS<'row'<'col-md-6 col-sm-6'i><'col-md-6 col-sm-6'p>>",
             pagingType: "full_numbers",
             columns: this.data.availableColumns,
