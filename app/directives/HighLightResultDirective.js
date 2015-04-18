63404c12fdc2dc26d3160a70d7c47f39fcd168b2
Justin Chasez <justin.nguyen@eztek.vn>
2015-04-18 17:36:13 +0700

    Finish highlight search result

---
+++ app/directives/HighLightResultDirective.js
@@ -0,0 +1,40 @@
+/**
+ * Created by justin on 4/18/15.
+ */
+app.registerDirective(function (container) {
+
+    function HighLightResultDirective() {
+        return {
+            link: function (scope, element, attrs) {
+
+                if (!attrs.highlightClass) {
+                    attrs.highlightClass = 'highlighted-result';
+                }
+
+                var replacer = function (match, item) {
+                    return '<span class="' + attrs.highlightClass + '">' + match + '</span>';
+                };
+
+                scope.$watch('keywords', function () {
+                    if (!scope.keywords || scope.keywords == '') {
+                        element.html(scope.highLightResult);
+                        return false;
+                    }
+
+                    var regex = new RegExp("([" + scope.keywords + "]+)", 'gi');
+
+                    // Find the words
+                    var html = scope.highLightResult.replace(regex, replacer);
+                    element.html(html);
+                });
+            },
+            replace: false,
+            scope: {
+                highLightResult: '=',
+                keywords: '='
+            }
+        };
+    }
+
+    return HighLightResultDirective;
+});
