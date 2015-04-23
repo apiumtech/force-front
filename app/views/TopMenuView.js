eab46f6fee098065abb3afb80033b10840b94a4c
Joan LLenas <joan.llenas@forcemanager.net>
2015-04-23 10:54:07 +0200

    WAR-310 menu web3 remake completed. web3 menu in web2 WIP.

--- app/views/TopMenuView.js
+++ app/views/TopMenuView.js
@@ -13,7 +13,8 @@ app.registerView(function(container) {
 
         this.web3Urls = [
             "localhost",
-            "127.0.0.1"
+            "127.0.0.1",
+            "54.171.216.35"
         ];
 
         this.configureEvents();
@@ -21,19 +22,35 @@ app.registerView(function(container) {
 
     TopMenuView.WEB3_TEMPLATE_NAME = 'topMenuWeb3';
     TopMenuView.WEB2_TEMPLATE_NAME = 'topMenuWeb2';
+
     TopMenuView.prototype = Object.create(BaseView.prototype, {});
 
     TopMenuView.prototype.configureEvents = function () {
         this.fn.getMenuTemplateName = this.getMenuTemplateName.bind(this);
+        this.fn.onInit = this.onInit.bind(this);
+    };
+
+    TopMenuView.prototype.onInit = function () {
+        this.inWeb3() ? this.onInitWeb3() : this.onInitWeb2();
+    };
+
+    TopMenuView.prototype.onInitWeb3 = function () {
+    };
+
+    TopMenuView.prototype.onInitWeb2 = function () {
     };
 
     TopMenuView.prototype.getMenuTemplateName = function () {
+        return this.inWeb3() ? TopMenuView.WEB3_TEMPLATE_NAME : TopMenuView.WEB2_TEMPLATE_NAME;
+    };
+
+    TopMenuView.prototype.inWeb3 = function () {
         var self = this;
         var shouldBeWeb3 = this.web3Urls.filter(function(item){
                 var found = self.$window.location.href.indexOf(item) > -1;
                 return found;
             }).length > 0;
-        return shouldBeWeb3 ? TopMenuView.WEB3_TEMPLATE_NAME : TopMenuView.WEB2_TEMPLATE_NAME;
+        return shouldBeWeb3;
     };
 
     TopMenuView.newInstance = function($scope, $model, $presenter, $window, $viewRepAspect, $logErrorAspect) {
--- test/src/views/TopMenuViewTestSpec.js
+++ test/src/views/TopMenuViewTestSpec.js
@@ -13,20 +13,51 @@ describe("TopMenuView", function(){
         expect(view.configureEvents).toHaveBeenCalled();
     });
 
-    it('should get the correct template name', function(){
-        $window = {
-            location: {
-                href: "http://localhost:8081"
-            }
-        };
+
+    it('should know if we are in web2 or inweb3', function(){
+        $window = {location: {href: ""}};
         var view = exerciseCreateView();
 
-        view.web3Urls = ["localhost", "127.0.0.1"];
-        expect(view.getMenuTemplateName()).toBe(TopMenuView.WEB3_TEMPLATE_NAME);
+        view.web3Urls = ["localhost", "127.0.0.1", "54.171.216.35"];
+
+        $window.location.href = "http://localhost:8081/";
+        expect(view.inWeb3()).toBe(true);
+
+        $window.location.href = "http://54.171.216.35:8081/";
+        expect(view.inWeb3()).toBe(true);
 
         $window.location.href = "https://web.forcemanager.net/";
+        expect(view.inWeb3()).toBe(false);
+    });
+
+    it('should get the correct template name', function(){
+        var view = exerciseCreateView();
+
+        var spyInWeb3 = spyOn(view, "inWeb3");
+
+        spyInWeb3.and.returnValue(true);
+        expect(view.getMenuTemplateName()).toBe(TopMenuView.WEB3_TEMPLATE_NAME);
+
+        spyInWeb3.and.returnValue(false);
         expect(view.getMenuTemplateName()).toBe(TopMenuView.WEB2_TEMPLATE_NAME);
     });
+
+    it('should call the correct onInit', function(){
+        var view = exerciseCreateView();
+
+        var spyInWeb3 = spyOn(view, "inWeb3");
+
+        spyInWeb3.and.returnValue(true);
+        spyOn(view, "onInitWeb3");
+        view.onInit();
+        expect(view.onInitWeb3).toHaveBeenCalled();
+
+        spyInWeb3.and.returnValue(false);
+        spyOn(view, "onInitWeb2");
+        view.onInit();
+        expect(view.onInitWeb2).toHaveBeenCalled();
+    });
+
 
 
 });
