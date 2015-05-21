9dc076cc9f71409ad1fc0797d8c791797ea0de37
Thuy <thuy.le@apiumtech.com>
2015-05-21 13:53:32 +0700

    Adding unit tests for conversion page

---
+++ test/src/modules/saleAnalytics/conversion/ConversionModelTestSpec.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Justin on 1/5/2015.
+ */
+define([
+    'modules/saleAnalytics/conversion/ConversionModel'
+], function (ConversionModel) {
+    'use strict';
+    describe("ConversionModel", function () {
+        var sut,
+            widgetService = {
+                getWidgetsForPage: function () {
+                }
+            },
+            storageService = {
+                store: function () {
+                },
+                retrieve: function () {
+                },
+                remove: function () {
+                }
+            };
+
+        beforeEach(function () {
+            sut = ConversionModel.newInstance(widgetService, storageService);
+        });
+        function fakeAjax() {
+            return {
+                then: function (a) {
+                    a({data: null});
+                    return fakeAjax();
+                },
+                fail: function (a) {
+                    a();
+                }
+            }
+        }
+
+        describe("sut.pageName", function () {
+            it("should be 'conversion'", function () {
+                expect(sut.pageName).toEqual('conversion');
+            });
+        });
+
+        describe("_getWidgets", function () {
+            it("should call getWidgetsForPage from service", function () {
+                spyOn(widgetService, 'getWidgetsForPage').and.returnValue(fakeAjax());
+                sut._getWidgets();
+                expect(widgetService.getWidgetsForPage).toHaveBeenCalledWith(sut.pageName);
+            });
+        });
+    });
+
+});
