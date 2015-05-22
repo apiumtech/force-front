d3c5765f358f185c7c205358f0811bd707c73116
Justin Chasez <justinchasez04121990@gmail.com>
2015-05-22 15:01:52 +0700

    Adding generate module scaffolding

---
+++ grunt/tasks/genModule.js
@@ -0,0 +1,20 @@
+/**
+ * Created by justin on 5/22/15.
+ */
+module.exports = function (grunt) {
+    grunt.registerTask('genModule', function (modulePath, featureName) {
+        if (!modulePath || !featureName)
+            throw new Error("Module Path and Feature Name must be specified");
+
+        [
+            'Controller',
+            'Model',
+            'View',
+            'Presenter'
+        ].forEach(function (type) {
+                var classPath = 'modules/' + modulePath + '/' + featureName + type;
+
+                grunt.task.run(['gentest:' + classPath]);
+            });
+    });
+};
