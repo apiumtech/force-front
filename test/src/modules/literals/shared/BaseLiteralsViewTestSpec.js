4c7282ca8902cf964f25cb70a722be374a3b6d08
Joan LLenas <joan.llenas@forcemanager.net>
2015-07-06 16:09:48 +0200

    add tests

--- test/src/modules/literals/shared/BaseLiteralsViewTestSpec.js
+++ test/src/modules/literals/shared/BaseLiteralsViewTestSpec.js
@@ -1,10 +1,12 @@
 define([
-	'modules/literals/shared/BaseLiteralsView'
-], function(BaseLiteralsView) {
+	'modules/literals/shared/BaseLiteralsView',
+	'modules/literals/shared/BaseLiteralsModel',
+    'shared/services/bus/ScrollEventBus'
+], function(BaseLiteralsView, BaseLiteralsModel, ScrollEventBus) {
 	'use strict';
 
-    function exerciseCreateView(scope, model, presenter){
-        return new BaseLiteralsView(scope, model, presenter);
+    function exerciseCreateView(model, presenter){
+        return new BaseLiteralsView(mockAngularScope(), model, presenter);
     }
 
 	describe('LiteralsView', function() {
@@ -14,6 +16,18 @@ define([
                 spyOn(BaseLiteralsView.prototype, 'configureEvents').and.callThrough();
                 var sut = exerciseCreateView();
                 expect(sut.configureEvents).toHaveBeenCalled();
+            });
+            it('should listen to ScrollEventBus.onScrolledToBottom', function () {
+                var testCallback;
+                spyOn(ScrollEventBus, "onScrolledToBottom").and.callFake(function(callback){
+                    testCallback = function(){
+                        callback();
+                    };
+                });
+                spyOn(BaseLiteralsView.prototype, "onPageScrolledToBottom");
+                var sut = exerciseCreateView();
+                testCallback();
+                expect(sut.onPageScrolledToBottom).toHaveBeenCalled();
             });
         });
 
@@ -29,6 +43,67 @@ define([
             });
         });
 
+        describe("onDisposing", function () {
+            var sut, model;
+            beforeEach(function () {
+                model = mock(BaseLiteralsModel);
+                spyOn(ScrollEventBus, "dispose");
+                sut = exerciseCreateView(model);
+                sut.disposer = function(){};
+            });
+            it("should call event's onDisposing", function () {
+                spyOn(sut.event, "onDisposing");
+                sut.onDisposing();
+                expect(sut.event.onDisposing).toHaveBeenCalled();
+            });
+            it("should dispose ScrollEventBus", function () {
+                sut.onDisposing();
+                expect(ScrollEventBus.dispose).toHaveBeenCalled();
+            });
+            it("should dispose $destroy handler", function () {
+                spyOn(sut, "disposer");
+                sut.onDisposing();
+                expect(sut.disposer).toHaveBeenCalled();
+            });
+            it("should resetScrollState", function () {
+                spyOn(sut, "resetScrollState");
+                sut.onDisposing();
+                expect(sut.resetScrollState).toHaveBeenCalled();
+            });
+        });
+
+        describe("showError", function () {
+            it('should set curretError', function () {
+                var sut = exerciseCreateView();
+                sut.showError("an error");
+                expect(sut.data.currentError).toBe("an error");
+            })
+        });
+
+        describe("onPageScrolledToBottom", function () {
+            it("should call event's nextPage when isPagerActive is true", function () {
+                var sut = exerciseCreateView();
+                spyOn(sut.event, "nextPage");
+                sut.isPagerActive = true;
+                sut.onPageScrolledToBottom();
+                expect(sut.event.nextPage).toHaveBeenCalled();
+            });
+            it("should not call event's nextPage when isPagerActive is false", function () {
+                var sut = exerciseCreateView();
+                spyOn(sut.event, "nextPage");
+                sut.isPagerActive = false;
+                sut.onPageScrolledToBottom();
+                expect(sut.event.nextPage).not.toHaveBeenCalled();
+            });
+        });
+
+        describe("resetScrollState", function () {
+            xit("should reset previosly set scroll state", function () {
+                var sut = exerciseCreateView();
+                $('body').css('min-height', 1900);
+                $('html, body').scrollTop( 0 );
+            });
+        })
 
 	});
 });
