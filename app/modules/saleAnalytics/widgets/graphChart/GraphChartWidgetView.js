fa1195a84da8951bf6b57332d0d9a7e9f5c4c50f
Joan Llenas <joan.llenas@forcemanager.net>
2016-02-22 13:10:48 +0100

    WAR-933

--- app/modules/saleAnalytics/widgets/graphChart/GraphChartWidgetView.js
+++ app/modules/saleAnalytics/widgets/graphChart/GraphChartWidgetView.js
@@ -433,33 +433,6 @@ define([
             maxValue: maxDate
         };
 
-        /*
-         self.chartOptions.hAxis = {
-         viewWindow: {
-         min: minDate,
-         max: maxDate
-         },
-         gridlines: {
-         count: -1,
-         units: {
-         years: {format: ['yyyy', 'yy']},
-         months: {format: ['d/M/yy', 'M/yy', 'M']},
-         days: {format: ['d/M/yy', 'd/M', 'd']}
-         },
-         color: '#666666'
-         },
-         minorGridlines: {
-         units: {
-         months: {format: ['M/yy', 'M']},
-         days: {format: ['d/M', 'd']},
-         hours: {format: [""]}
-         },
-         color: '#DDDDDD'
-         }
-         };
-        */
-
-
         if( isHours() ){
             self.chartOptions.bar = {groupWidth: "75%"};
             var hourTicks = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23].map( function(n){
