5f8a57e930b43def1e84775987067663136e9368
Joan Llenas <joan.llenas@forcemanager.net>
2016-02-23 18:36:06 +0100

    WAR-938

--- app/modules/saleAnalytics/filters/userFilter/UserFilterView.js
+++ app/modules/saleAnalytics/filters/userFilter/UserFilterView.js
@@ -202,6 +202,19 @@ define([
             self.checkSelectAllState();
             self.fn.applyUserFilter();
         };
+
+
+        $.fn.disableScroll = function() {
+            window.oldScrollPos = $(window).scrollTop();
+
+            $(window).on('scroll.scrolldisabler',function ( event ) {
+                $(window).scrollTop( window.oldScrollPos );
+                event.preventDefault();
+            });
+        };
+        $.fn.enableScroll = function() {
+            $(window).off('scroll.scrolldisabler');
+        };
     };
 
 
