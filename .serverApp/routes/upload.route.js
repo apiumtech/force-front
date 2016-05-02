c0b077954d372465cb4aefa5fddde9bb303d5ec4
Joan Llenas <joan.llenas@forcemanager.net>
2016-05-02 11:31:48 +0200

    Removed unused files

--- .serverApp/routes/upload.route.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by justin on 3/20/15.
- */
-
-var UploadController = require('../controllers/uploadController');
-module.exports = function (app) {
-    app.post('/api/upload', UploadController.upload);
-    app.post('/api/uploadDocument', UploadController.uploadDocument);
-};
