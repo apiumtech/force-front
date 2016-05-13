0184283c6a25fb29824a3ac4fc9fac6eaffa964e
Joan Llenas <joan.llenas@forcemanager.net>
2016-05-13 12:43:20 +0200

    Added profile dropdown in the header like winter app

--- app/core/topMenu/TopMenuWeb4Controller.js
+++ app/core/topMenu/TopMenuWeb4Controller.js
@@ -4,16 +4,16 @@ define([
 ], function (app, TopMenuWeb4View) {
     'use strict';
 
-    function TopMenuWeb4Controller($scope) {
-        TopMenuWeb4Controller.configureView($scope);
+    function TopMenuWeb4Controller($scope, $rootScope) {
+        TopMenuWeb4Controller.configureView($scope, $rootScope);
     }
 
-    TopMenuWeb4Controller.configureView = function ($scope) {
-        this.view = TopMenuWeb4View.newInstance($scope);
+    TopMenuWeb4Controller.configureView = function ($scope, $rootScope) {
+        this.view = TopMenuWeb4View.newInstance($scope, $rootScope);
         this.view.show();
     };
 
-    app.register.controller('TopMenuWeb4Controller', ['$scope', TopMenuWeb4Controller]);
+    app.register.controller('TopMenuWeb4Controller', ['$scope', '$rootScope', TopMenuWeb4Controller]);
 
     return TopMenuWeb4Controller;
 });
